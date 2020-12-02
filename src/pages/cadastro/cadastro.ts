4edffa9e34386aa44dd6b7f03cd65c790561ae12
Romulo de Oliveira Azevedo DevOps <romuloazevedobr@gmail.com>
2020-12-01 22:17:37 -0300

    Update cadastro.ts

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -24,20 +24,39 @@ export class CadastroPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public formbuilder: FormBuilder
+    public formbuilder: FormBuilder,
+    public afAuth: AngularFireAuth
     ) {
       this.registerForm = this.formbuilder.group({
         name: [null, [Validators.required, Validators.minLength(5)]],
         email: [null, [Validators.required, Validators.email]],
         password: [null, [Validators.required, Validators.minLength(5)]],
-        confirmPassword: [null, [Validators.required, Validators.minLength(5)]]
+        confirmPassword: [null, [Validators.required, Validators.minLength(5), ValidateConfirmPassword]]
         
 
       })
   }
 
  submitForm(){
-   console.log(this.registerForm.value);
+  //  console.log(this.registerForm.value);
+  this.afAuth.auth.createUserWithEmailAndPassword(this.registerForm.value.email, this.registerForm.value.passowrd)
+  .then((response) => {
+    this.presentAlert();
+    this.navCtrl.push("cadastro");
+    console.log("Usuário criado conforme solicitado!");
+  })
+  .catch((error) => {
+    console.log("Erro ao criar usuário", error);
+  })
+
+  presentAlert(){
+    let alert = this.alertCtrl.create({
+      title: "Usuário",
+      subTitle: "Usuário cadastrado com sucesso",
+      buttons: ["ok"]
+    })
+    alert.present();
+  }
  }
 
 }
